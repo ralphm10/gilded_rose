f67a3ccb3d6cc32f4d00338106abd6ecf18f9456
Ralph Mallett <rmallett12@gmail.com>
2020-09-09 11:18:00 +0100

    Add tests for all conditions except backstage passes

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -2,6 +2,8 @@ require_relative 'item'
 
 class GildedRose
 
+  MAXIMUM_QUALITY = 50
+
   def initialize(items)
     @items = items
   end
@@ -11,21 +13,21 @@ class GildedRose
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
         if item.quality > 0
           if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
+            item.quality -= 1
           end
         end
       else
-        if item.quality < 50
+        if item.quality < MAXIMUM_QUALITY
           item.quality = item.quality + 1
           if item.name == "Backstage passes to a TAFKAL80ETC concert"
             if item.sell_in < 11
               if item.quality < 50
-                item.quality = item.quality + 1
+                item.quality += 1
               end
             end
             if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
+              if item.quality < MAXIMUM_QUALITY
+                item.quality += 1
               end
             end
           end
@@ -39,14 +41,14 @@ class GildedRose
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
             if item.quality > 0
               if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
+                item.quality -= 1
               end
             end
           else
             item.quality = item.quality - item.quality
           end
         else
-          if item.quality < 50
+          if item.quality < MAXIMUM_QUALITY
             item.quality = item.quality + 1
           end
         end
--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -8,6 +8,30 @@ describe GildedRose do
       GildedRose.new(items).update_quality()
       expect(items[0].name).to eq "foo"
     end
+    it "does not make the quality negative" do
+      items = [Item.new("foo", 0, 0)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 0
+    end
+    it "reduces quality twice as fast after sell by" do
+      items = [Item.new("foo", 0, 4)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 2
+    end
+    it "increases quality of aged Brie" do
+      items = [Item.new("Aged Brie", 2, 0)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 1
+    end
+    it "does not increase quality above 50" do
+      items = [Item.new("Aged Brie", 0, 50)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 50
+    end
+    it "does not change quality of Sulfuras items" do
+      items = [Item.new("Sulfuras, Hand of Ragnaros", 0, 10)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 10
+    end
   end
-
 end
